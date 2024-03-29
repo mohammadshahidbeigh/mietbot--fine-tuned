89b1ee68022337072cd91a847b03e84debd96349
Mohammad Shahid Beigh <85876937+mohammadshahidbeigh@users.noreply.github.com>
2024-03-29 19:36:04 +0530

    Delete env.js

--- env.js
+++
@@ -1,8 +0,0 @@
-export const process = {
-  env: {
-    OPENAI_API_KEY: "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", // Your OpenAI API Key
-    FIREBASE_DATABASE_URL:
-      "https://mietbot-49caf-default-rtdb.asia-southeast1.firebasedatabase.app/", // Your Firebase URL
-  },
-};
-
